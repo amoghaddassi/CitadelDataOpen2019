d0266bdaea31cba724510ebb460bf6040f78e4c4
Aram Moghaddassi <amoghaddassi@berkeley.edu>
2019-02-23 08:31:34 -0800

    parsing for dataset1

--- utils.py
+++ utils.py
@@ -12,3 +12,24 @@ def import_datatable(i):
 def clean_datatable_1(df):
     codes = df["energy_code"]
 
+def split_energy_code(code):
+	source = code[:2]
+	sector = code[2:4]
+	value_unit = code[4]
+	return source, sector, value_unit
+
+def parse_dataset1(df):
+	energy_code_col = df["energy_code"]
+	sources = []
+	sectors = []
+	value_units = []
+	for code in energy_code_col:
+		source, sector, value_unit = split_energy_code(code)
+		sources.append(source)
+		sectors.append(sector)
+		value_units.append(value_unit)
+
+	df["energy_source"] = sources
+	df["sector"] = sectors
+	df["value_unit"] = value_units
+	return df
